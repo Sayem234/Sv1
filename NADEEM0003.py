05603d2f6d0a4fc0ad233fb515be8c82f21a1dae
Sayem234 <88239049+Sayem234@users.noreply.github.com>
2022-03-06 12:50:02 +0600

    Update NADEEM0003.py

--- NADEEM0003.py
+++ NADEEM0003.py
@@ -47,7 +47,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com/Sayem234/Sv1/server.txt').text
+    r = requests.get('https://raw.githubusercontent.com/Sayem234/Sv1/main/server.txt').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
